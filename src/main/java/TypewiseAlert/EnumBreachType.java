058bedc33b6569fe98d3041167f13ff603a9a0c3
Sreyanka <35264560+Sreyanka@users.noreply.github.com>
2023-01-13 17:13:28 +0530

    Update EnumBreachType.java

--- src/main/java/TypewiseAlert/EnumBreachType.java
+++ src/main/java/TypewiseAlert/EnumBreachType.java
@@ -1,13 +1,13 @@
 package TypewiseAlert;
 
-public enum BreachType {
+public enum EnumBreachType {
 	      NORMAL("Temperature is normal"),
 	      TOO_LOW("Hi, the temperature is too low"),
 	      TOO_HIGH("Hi, the temperature is too high");
 
 	public String message;
 	
-		BreachType(String message) {
+		EnumBreachType(String message) {
 			this.message=message;
 		}
  public String getMessage() {
