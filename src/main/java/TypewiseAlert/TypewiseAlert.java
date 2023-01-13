fb3611d32c715dbe088ba3e864b6abc1604350a6
Sreyanka <35264560+Sreyanka@users.noreply.github.com>
2023-01-13 17:24:40 +0530

    Update TypewiseAlert.java

--- src/main/java/TypewiseAlert/TypewiseAlert.java
+++ src/main/java/TypewiseAlert/TypewiseAlert.java
@@ -2,14 +2,14 @@ package TypewiseAlert;
 
 public class TypewiseAlert 
 {
-    public static void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+    public static void checkAndAlert(EnumAlert alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
 
-		BreachType breachType = BreachAnalyzer.classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+		EnumBreachType breachType = BreachAnalyzer.classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
  
 		if(alertTarget==AlertTarget.TO_CONTROLLER) {
-			AlertTarget.sendToController(breachType);
+			EnumAlert.sendToController(breachType);
 		}else {
-			AlertTarget.sendToEmail(breachType);
+			EnumAlert.sendToEmail(breachType);
 		}
 	
 	}
